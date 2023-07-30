354491a3dd250b4d4c9fdb31524702ae21fcc149
A. Nithin <63183221+NITHIN06@users.noreply.github.com>
2023-07-30 11:50:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    const addButton = document.getElementsByClassName('toggle-button')[0]
+    const removeButton = document.getElementsByClassName('btn')[0]
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+    addButton.addEventListener('click', function() {
+        console.log('setsgsbbxfthdhd')
+        navbarLinks.classList.add('active');
+        on();
+    })
+    removeButton.addEventListener('click', function() {
+        navbarLinks.classList.remove('active')
+        off();
+    })      
+}
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+    navbarLinks.classList.remove('active')
+}
