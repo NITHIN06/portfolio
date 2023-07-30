79946df2bc4d9227c7619ad95ea6193a45b67c0a
A. Nithin <63183221+NITHIN06@users.noreply.github.com>
2023-07-30 19:39:48 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.onload = function(){
     const addButton = document.getElementsByClassName('toggle-button')[0]
     const removeButton = document.getElementsByClassName('btn')[0]
     const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-    addButton.addEventListener('click', function() {
-        console.log('setsgsbbxfthdhd')
+    addButton.addEventListener('click', function(event) {
         navbarLinks.classList.add('active');
         on();
+        event.preventDefault();
     })
     removeButton.addEventListener('click', function() {
         navbarLinks.classList.remove('active')
